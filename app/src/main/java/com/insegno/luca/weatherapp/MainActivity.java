b9d4d35b20793696764794d960e6b86dfc866669
luca <luca.errani1@gmail.com>
2019-03-20 19:43:10 +0100

    First commit

---
+++ app/src/main/java/com/insegno/luca/weatherapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.insegno.luca.weatherapp;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+}
